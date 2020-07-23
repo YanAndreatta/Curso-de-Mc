48f43fb93f2dd39a9b232a7fafacb2261a83d4ff
yanandreatta <andreattadelima@gmail.com>
2020-07-22 23:06:20 -0300

    Estado e cidade

---
+++ src/main/java/com/yan/cursomc/repositories/CidadeRepository.java
@@ -0,0 +1,11 @@
+package com.yan.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.yan.cursomc.domain.Cidade;
+
+@Repository
+public interface CidadeRepository extends JpaRepository<Cidade, Integer> {
+	
+}
---
+++ src/main/java/com/yan/cursomc/repositories/EstadoRepository.java
@@ -0,0 +1,11 @@
+package com.yan.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.yan.cursomc.domain.Estado;
+
+@Repository
+public interface EstadoRepository extends JpaRepository<Estado, Integer> {
+	
+}
